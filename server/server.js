3cd10ed9e072cbef326667215ace9885011d795f
terahje <terahjequinn@gmail.com>
2021-01-20 18:35:04 -0700

    updates to try to get heroku running

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.get('*', (req, res) => {
 });
 
 db.once('open', () => {
-  app.listen(PORT, () => {
+  app.listen(process.env.PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
